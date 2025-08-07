a45892e97f33e5f78a452fd4e25fcc909fb7ad5c
anujmish1229 <anujmish1229@gmail.com>
2025-08-06 22:20:18 -0400

    Update get-gallery.js

--- netlify/functions/get-gallery.js
+++ netlify/functions/get-gallery.js
@@ -2,19 +2,17 @@ import { Client } from 'pg'
 
 export default async (req, res) => {
   const client = new Client({
-    connectionString: process.env.NETLIFY_DATABASE_URL, 
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    connectionString: process.env.NETLIFY_DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   })
 
   try {
     await client.connect()
     const result = await client.query('SELECT path FROM images ORDER BY id')
-    await client.end()
-
-    return res.status(200).json(result.rows)
+    res.status(200).json(result.rows)
   } catch (err) {
-    return res.status(500).json({ error: err.message })
+    res.status(500).json({ error: err.message })
+  } finally {
+    await client.end()
   }
 }
