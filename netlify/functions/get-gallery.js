ad9962afe733bceb7f0ae07939c22709bb50e0e0
anujmish1229 <anujmish1229@gmail.com>
2025-08-06 21:47:24 -0400

    a bunch of stuff

---
+++ netlify/functions/get-gallery.js
@@ -0,0 +1,20 @@
+import { Client } from 'pg'
+
+export default async (req, res) => {
+  const client = new Client({
+    connectionString: process.env.NEON_DB_URL, // Store this in Netlify environment variables
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  })
+
+  try {
+    await client.connect()
+    const result = await client.query('SELECT path FROM gallery ORDER BY id')
+    await client.end()
+
+    return res.status(200).json(result.rows)
+  } catch (err) {
+    return res.status(500).json({ error: err.message })
+  }
+}
